68df72d28479458f7b88fc1904b610c10cfc5743
z006290 <sivaa.sm@gmail.com>
2020-01-01 23:29:36 +0530

    Added the DI example

---
+++ src/test/java/guru/springframework/controllers/GetterInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.GreetingServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GetterInjectedControllerTest {
+
+    private GetterInjectedController getterInjectedController;
+
+    @Before
+    public void setUp() throws Exception{
+        getterInjectedController=new GetterInjectedController();
+        getterInjectedController.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void testGreeting() throws Exception{
+        assertEquals(GreetingServiceImpl.HELLO_GURUS,getterInjectedController.sayHello());
+    }
+}
